a68581871a2d51263ae616105fbd18294d054b87
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-12-15 01:24:15 +0900

    [UPDATE] UIManager - 코드 업데이트

--- UIManager를 통한 UI 관리/UIManager.cs
+++ UIManager를 통한 UI 관리/UIManager.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UI_Popup;
 
 public class UIManager : Singleton<UIManager>
 {
     // Resources/Prefabs/UI 안의 UI 컴포넌트 프리팹들을 저장하는 딕셔너리
-    private Dictionary<string, UI_Base> _uiDic = new Dictionary<string, UI_Base>();
-    private Dictionary<string, UI_Popup> _uiPopupDic = new Dictionary<string, UI_Popup>();
+    private readonly Dictionary<string, UI_Base> _uiDic = new Dictionary<string, UI_Base>();
+    private readonly Dictionary<string, UI_Popup> _uiPopupDic = new Dictionary<string, UI_Popup>();
+
+    private Stack<UI_Base> _uiOnScreen = new Stack<UI_Base>();
 
     public T GetUIComponent<T>() where T : UI_Base
     {
@@ -15,7 +15,7 @@ public class UIManager : Singleton<UIManager>
         if (!_uiDic.ContainsKey(key))
         {
             // 요청 시점에 해당 UI 컴포넌트가 없다면 Load
-            GameObject prefab = Resources.Load<GameObject>($"Prefabs/UI/{key}");
+            GameObject prefab = Resources.Load<GameObject>($"{Strings.Prefabs.UI_PREFAB_PATH}{key}");
             if (!prefab)
             {
 #if UNITY_EDITOR
@@ -42,7 +42,7 @@ public class UIManager : Singleton<UIManager>
         if (!_uiDic.ContainsKey(key))
         {
             // 요청 시점에 해당 UI 컴포넌트가 없다면 Load
-            GameObject prefab = Resources.Load<GameObject>($"Prefabs/UI/{key}");
+            GameObject prefab = Resources.Load<GameObject>($"{Strings.Prefabs.UI_PREFAB_PATH}{key}");
             if (!prefab)
             {
 #if UNITY_EDITOR
@@ -86,7 +86,7 @@ public class UIManager : Singleton<UIManager>
         if (!_uiPopupDic.ContainsKey(key))
         {
             // 요청 시점에 해당 UI 컴포넌트가 없다면 Load
-            GameObject prefab = Resources.Load<GameObject>($"Prefabs/UI/Popup/{key}");
+            GameObject prefab = Resources.Load<GameObject>($"{Strings.Prefabs.UI_POPUP_PREFAB_PATH}{key}");
             if (!prefab)
             {
 #if UNITY_EDITOR
@@ -109,5 +109,53 @@ public class UIManager : Singleton<UIManager>
         _uiPopupDic[key].ShowPopup(popupParameter);
 
         return _uiPopupDic[key] as T;
+    }
+
+    public void SetUIOnScreen(UI_Base ui, bool isOpen)
+    {
+        if (isOpen)
+        {
+            if (ui is UI_Dialog) return;
+            if (_uiOnScreen.Contains(ui)) return;
+
+            _uiOnScreen.Push(ui);
+        }
+        else
+        {
+            if (!_uiOnScreen.Contains(ui)) return;
+
+            while (_uiOnScreen.Peek() != ui)
+            {
+                _uiOnScreen.Pop();
+            }
+
+            _uiOnScreen.Pop();
+        }
+    }
+
+    public UI_Base GetCurrentUI()
+    {
+        if (_uiOnScreen.TryPeek(out UI_Base ui)) return ui;
+        return null;
+    }
+
+    public bool IsUIActivated<T>() where T : UI_Base
+    {
+        foreach (UI_Base ui in _uiOnScreen)
+        {
+            if (ui is T) return true;
+            else continue;
+        }
+
+        return false;
+    }
+
+    public void CloseAllPopUps(bool isSound = false)
+    {
+        foreach (KeyValuePair<string, UI_Popup> popUp in _uiPopupDic)
+        {
+            if (popUp.Value.gameObject.activeSelf)
+                popUp.Value.CloseUI();
+        }
     }
 }
